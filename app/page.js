34ca59ac743f6fe909d16796af841d66695fe0aa
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-12-17 18:02:34 -0500

    Create getTickets method

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import TicketCard from './(components)/TicketCard'
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch('http://localhost:3000/api/Tickets', {
+      cache: 'no-store',
+    })
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.log("Failed to fetch tickets", error);
+  }
+}
+
+const Dashboard = async () => {
+
+  const tickets = await getTickets();
+
   return (
     <div className='p-5'>
       <div className='lg:grid grid-cols-2 xl:grid-cols-4'>
