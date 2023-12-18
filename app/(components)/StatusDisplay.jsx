027dfeadef45fe91269b08e31bca9d5ae0844a57
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-12-18 18:21:00 -0500

    Make StatusDisplay dynamic

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
 const StatusDisplay = ({status}) => {
+
+  const getColor = (status) => {
+    switch (status) {
+      case 'Open':
+        return 'bg-green-200'
+      case 'In Progress':
+        return 'bg-yellow-200'
+      case 'Closed':
+        return 'bg-red-200'
+      default:
+        return 'bg-green-200'
+    }
+  }
+
+
   return (
-    <div className='inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200'>
+    <div className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(status)}`}>
       {status}
     </div>
   )
