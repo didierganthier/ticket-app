9c9cafddcd29d18e26aef1b8992ae59d94f2554e
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-12-18 18:22:40 -0500

    Change display

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -4,11 +4,11 @@ const StatusDisplay = ({status}) => {
 
   const getColor = (status) => {
     switch (status) {
-      case 'Open':
+      case 'open':
         return 'bg-green-200'
-      case 'In Progress':
+      case 'in-progress':
         return 'bg-yellow-200'
-      case 'Closed':
+      case 'closed':
         return 'bg-red-200'
       default:
         return 'bg-green-200'
