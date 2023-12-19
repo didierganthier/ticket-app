384119cf7aeeda7bb994bca64aec38c8e241a593
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-12-18 20:57:50 -0500

    Add Update Ticket functionality

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,9 +1,38 @@
 import TicketForm from '@/app/(components)/TicketForm'
 import React from 'react'
 
-const TicketPage = ({params}) => {
+const getTicketById = async (id) => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      cache: 'no-store',
+    });
+    
+    if(!res.ok) {
+      throw new Error('Faled to get Ticket');
+    }
+  
+    return res.json();
+}
+
+let updateTicketData = {};
+const TicketPage = async ({params}) => {
+
+  const EDITMODE = params.id === 'new' ? false : true;
+
+  if (EDITMODE) {
+    console.log('EDIT MODE');
+    updateTicketData = await getTicketById(params.id);
+    updateTicketData = updateTicketData.foundTicket;
+    console.log(updateTicketData);
+  } else {
+    updateTicketData = {
+      _id: 'new'
+    }
+    console.log("TicketData", updateTicketData);
+    console.log('NOT EDIT MODE');
+  }
+
   return (
-    <TicketForm/>
+    <TicketForm ticket={updateTicketData} />
   )
 }
 
--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -1,12 +1,43 @@
 import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req, {params}) {
-  try {
-    const { id } = params;
-    await Ticket.findByIdAndDelete(id);
+export async function GET(req, { params }) {
+    try {
+        const { id } = params;
 
-    return NextResponse.json({ message: "Ticket deleted successfully" }, { status: 200 });
+        const foundTicket = await Ticket.findOne({ _id: id });
+
+        return NextResponse.json({ foundTicket }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
+
+export async function PUT(req, { params }) {
+    try {
+      const { id } = params;
+  
+      const body = await req.json();
+      const ticketData = body.formData;
+  
+      const updateTicketData = await Ticket.findByIdAndUpdate(id, {
+        ...ticketData,
+      });
+  
+      return NextResponse.json({ message: "Ticket updated" }, { status: 200 });
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json({ message: "Error", error }, { status: 500 });
+    }
+  }
+
+
+export async function DELETE(req, { params }) {
+    try {
+        const { id } = params;
+        await Ticket.findByIdAndDelete(id);
+
+        return NextResponse.json({ message: "Ticket deleted successfully" }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
